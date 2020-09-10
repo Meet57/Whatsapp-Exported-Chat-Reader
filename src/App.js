fc0b2da140871d49b329c2e40755c7d38b79814f
Meet Patel <45942161+Meet57@users.noreply.github.com>
2020-09-10 18:18:27 +0530

    final

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
       <center>
 
           <h1> <span role="img" aria-label="emoji" >🚀</span> Whatsapp Txt Reader <span role="img" aria-label="emoji" >📱</span></h1>
-          <p>We Export chat when it is really important. but when we try to read it later i really becomes a tough job to read it line by line</p>
+          <p>We Export chat when it is really important. but when we try to read it later it really becomes a tough job to read it line by line</p>
           <p>So this is my Web App, which will help you to Read exported data chats in this</p>
           <p>Just copy paste that exported chat here and BOOM <span role="img" aria-label="emoji" >🚀</span> . You wil Have your chat well Designed and easy to understand</p>
         <SetMessage />
